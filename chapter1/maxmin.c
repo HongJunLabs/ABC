5c4493e59a05c152ade4d47ea775d632992762eb
HongJun Kim <weblue2@gmail.com>
2018-08-26 11:37:12 +0900

    2018-08-26 study

---
+++ chapter1/maxmin.c
@@ -0,0 +1,54 @@
+/* In file maxmin.c */
+
+#include <stdio.h>
+
+float maxmun(float x, float y);
+float minmun(float x, float y);
+void prn_info(void);
+
+int main(void)
+{
+	int i, n;
+	float max, min, x;
+	prn_info();
+	printf("Input n: ");
+	scanf("%d", &n);
+	printf("\nInput%d real numbers:", n);
+	scanf("%f", &x);
+	max = min = x;
+	
+	for(i = 2; i <= n; ++i)
+	{
+		scanf("%f", &x);
+		max = maxmun(max, x);
+		min = minmun(min, x);
+	}
+	printf("\n%s%11.3f\n%s%11.3f\n\n",
+		"Maximum value:", max,
+		"Minimum value:", min);
+	
+	return 0;
+}
+
+float maxmun(float x, float y)
+{
+	if(x > y)
+		return x;
+	else
+		return y;
+}
+
+float minmun(float x, float y)
+{
+	if(x < y)
+		return x;
+	else
+		return y;
+}
+
+void prn_info(void)
+{
+	printf("\n%s\n%s\n\n",
+		"This program reads an integer value for n, and then",
+		"processes n real number to find max and min values.");
+}
